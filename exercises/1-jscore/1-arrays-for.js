e3aff0db3b7e883060e102ccb2673a26d5946f69
bipasha39 <physiobipasha39@gmail.com>
2020-09-09 17:54:04 +0200

    extra 1

--- exercises/1-jscore/1-arrays-for.js
+++ exercises/1-jscore/1-arrays-for.js
@@ -5,6 +5,21 @@
 
 const mcTeam = ["angelo","vincent","carlos","henriette"];
 
+for (let i = 0; i < mcTeam.length; i++) {
+    console.log(mcTeam[i][0].toUpperCase() + mcTeam[i].substring(1))
+  }
+
+
+
+//  for (  i = 0; i < 4; i++) {
+//     console.log((mcTeam[i]).toUpperCase);
+//  }
+
+// const uppercased = mcTeam.map(name => name.toUpperCase());
+// console.log(uppercased);
+
+
+
 //YOUR CODE HERE
 
 
